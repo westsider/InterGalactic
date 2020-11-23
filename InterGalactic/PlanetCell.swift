e057c48f4ec9d9bc176e80746d9a0babca122193
Warren Hansen <whansen1@mac.com>
2020-11-23 11:33:58 -0800

    added planets

--- InterGalactic/PlanetCell.swift
+++ InterGalactic/PlanetCell.swift
@@ -20,8 +20,12 @@ class PlanetCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        planetImg.layer.cornerRadius = 10
         // Configure the view for the selected state
     }
 
+    func configureCell(planet: String) {
+        planetImg.image = UIImage(named: planet)
+        planetLabel.text = planet
+    }
 }
--- InterGalactic/PlanetPickerVC.swift
+++ InterGalactic/PlanetPickerVC.swift
@@ -9,22 +9,31 @@ import UIKit
 
 class PlanetPickerVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let planets = ["sun", "mercury", "venus", "earth", "earth-night", "moon", "mars", "jupiter", "saturn", "uranus", "neptune"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.contentInset = UIEdgeInsets(top: 10, left: 0, bottom: 0, right: 0)
     }
 
 }
 
 extension PlanetPickerVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return planets.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "planetCell", for: indexPath) as? PlanetCell {
+            cell.configureCell(planet:  planets[indexPath.row])
+            return cell
+        }
         return UITableViewCell()
     }
     
-    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
 }
